7ddd704da9b1c87be2ef265cfbe8998c0989de4b
Vasiliy <evtuth@gmail.com>
2023-03-30 20:11:41 +0300

    create task3

--- Task3.java
+++ Task3.java
@@ -6,11 +6,11 @@ public class Task3 {
         simpleNumbers();
     }
 
-    static void simpleNumbers(){
+    static void simpleNumbers() {
         for(int i = 2; i <= 1000; i++) {
             int count = 0;
             for(int j = 2; j <= i && count < 2; j++){
-                if (i%j== 0){
+                if (i%j== 0) {
                     count++;
                 }
         
